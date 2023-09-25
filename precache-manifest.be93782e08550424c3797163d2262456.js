3ca235f10e8f6cd619b06e0e1e754bdecbffc3f7
burak <88000841+buraaaaaak@users.noreply.github.com>
2023-09-25 22:00:48 +0300

    Add files via upload

---
+++ precache-manifest.be93782e08550424c3797163d2262456.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aed12af2984c2eed69b615e7af7af9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecff071423263e429d79",
+    "url": "/static/css/main.721d9ea0.chunk.css"
+  },
+  {
+    "revision": "0094fef0aa9cdd1d0a7f",
+    "url": "/static/js/2.ca09690e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.ca09690e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecff071423263e429d79",
+    "url": "/static/js/main.f6c76819.chunk.js"
+  },
+  {
+    "revision": "349dbad2011225b191fa",
+    "url": "/static/js/runtime-main.5f51ba00.js"
+  }
+]);
